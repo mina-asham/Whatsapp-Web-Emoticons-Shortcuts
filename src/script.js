d24b8ea8ed4034e4fee0d384baf2dcf2ba33d679
Mina Asham <mina.asham@hotmail.com>
2015-05-03 18:24:56 +0100

    Adding some extra shortcuts

--- src/script.js
+++ src/script.js
@@ -38,7 +38,9 @@ var shortcuts = {
 	'b)'   : '<img alt="😎" class="emoji emojiordered1167" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">',
 	':|'   : '<img alt="😐" class="emoji emojiordered1169" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">',
 	':$'   : '<img alt="" class="emoji emojiordered1204" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">',
-	'xd'   : '<img alt="" class="emoji emojiordered1169" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">'
+	'xd'   : '<img alt="" class="emoji emojiordered1169" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">',
+	':*'   : '<img alt="" class="emoji emojiordered1177" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">',
+	'::*'  : '<img alt="" class="emoji emojiordered1166" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">'
 };
 
 $(document).ready(function () {
